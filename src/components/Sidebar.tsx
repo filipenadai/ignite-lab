45aa083e220b4c2baa182f39a6b8a5e65cf24439
filipenadai <f.nadai05@gmail.com>
2022-06-24 16:54:36 -0300

    finish second lesson

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,32 @@
-import { Lesson } from "./Lesson";
+import { gql, useQuery } from '@apollo/client';
 
-export function Sidebar() {
+import { Lesson } from './Lesson';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      slug
+      title
+      lessonType
+      availableAt
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    slug: string;
+    title: string;
+    lessonType: 'live' | 'class';
+    availableAt: string;
+  }[];
+}
+
+export function Sidebar(): JSX.Element {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -8,13 +34,16 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        {data?.lessons.map(lesson => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title}
+            slug={lesson.slug}
+            type={lesson.lessonType}
+            availableAt={new Date(lesson.availableAt)}
+          />
+        ))}
       </div>
     </aside>
-  )
+  );
 }
